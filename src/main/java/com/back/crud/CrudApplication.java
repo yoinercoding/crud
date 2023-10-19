523e07081d3fb8bd2e237b1c5151266e26f7ba38
yoinercoding <yoinercoding@gmail.com>
2023-10-19 12:17:26 -0300

    update

--- src/main/java/com/back/crud/CrudApplication.java
+++ src/main/java/com/back/crud/CrudApplication.java
@@ -12,9 +12,10 @@ public class CrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
+
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
+		return new WebMvcConfigurer() { 
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/*").allowedOrigins("http://localhost:8080");
