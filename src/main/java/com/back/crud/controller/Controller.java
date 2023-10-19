51f1e43e021091e2e33cbb468a983329868c9095
yoinercoding <yoinercoding@gmail.com>
2023-10-19 12:17:12 -0300

    param -> path

--- src/main/java/com/back/crud/controller/Controller.java
+++ src/main/java/com/back/crud/controller/Controller.java
@@ -4,7 +4,6 @@ import java.util.List;
 import com.back.crud.entity.Persona;
 import com.back.crud.services.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -14,28 +13,29 @@ public class Controller {
 	@Autowired
     PersonaService service;
 	
-	@GetMapping("/api/person")
+	@GetMapping("/api/person/")
 	private List<Persona>getAllPersona() {
         return service.listar();
 	}
 	
-	@PostMapping("/api/person/add")
+	@PostMapping("/api/person/")
     public Persona agregar(@RequestBody Persona p){
         return service.add(p);
     }
 
-    @GetMapping({"/api/person/id"})
-    public Persona listarId(@Param("id") int id){
+    @GetMapping(path = "/api/person/{id}")
+    public Persona listarId(@PathVariable("id") int id){
         return service.listarId(id);
     }
-    @PutMapping({"/api/person/edit"})
-    public Persona editar(@RequestBody Persona p, @Param("id") int id){
+
+    @PutMapping(path = "/api/person/{id}")
+    public Persona editar(@RequestBody Persona p, @PathVariable("id") int id){
         p.setId(id);
         return service.edit(p);
     }
 
-    @DeleteMapping({"/api/person/delete"})
-    public Persona delete(@Param("id") int  id){
+    @DeleteMapping(path = "/api/person/{id}")
+    public Persona delete(@PathVariable("id") int  id){
         return service.delete(id);
     }
 }
