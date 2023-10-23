9a0a71a0f872827390cdca764b9b65076b73a85b
yoinercoding <yoinercoding@gmail.com>
2023-10-23 11:03:19 -0300

    modified edit and delete update

--- src/app/components/edit/editar.component.ts
+++ src/app/components/edit/editar.component.ts
@@ -3,37 +3,29 @@ import { Router } from '@angular/router';
 import { ServiceService } from 'src/app/Service/service.service';
 import { Persona } from 'src/app/model/Persona';
 
-
 @Component({
   selector: 'app-editar',
   templateUrl: './editar.component.html',
   styleUrls: ['./editar.component.css']
 })
+
 export class EditarComponent {
 
-  persona :Persona=new Persona();
+  persona:Persona = new Persona();
   constructor(private router:Router,private service:ServiceService) { }
 
   ngOnInit() {
-    this.Editar();
   }
 
-  Editar(){
-    let id=localStorage.getItem("id");
-    if (id != undefined){
-      this.service.getPersonaId(parseInt(id))
-      .subscribe(data=>{
-        this.persona=data;
-      })
+  Actualizar(){
+    let id = localStorage.getItem("id");
+    if (id != undefined) {
+      this.persona.id = parseInt(id);
+      this.service.updatePersona(this.persona).subscribe(data => {
+        this.persona = data;
+        alert("Se actualizó con exito!");
+        this.router.navigate(["listar"]);
+      })        
     }
-  }
-
-  Actualizar(persona:Persona){
-    this.service.updatePersona(persona)
-    .subscribe(data=>{
-      this.persona=data;
-      alert("Se Actualizo con Exito...!!!");
-      this.router.navigate(["listar"]);
-    })
   }
 }
